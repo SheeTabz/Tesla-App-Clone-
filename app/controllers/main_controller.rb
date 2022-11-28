20ddaa84bb50d57493a371a4a7b0995bc88b4d7e
SheeTabz <wmuriithitabitha@gmail.com>
2022-11-28 22:52:33 +0300

    Main controller for root template

---
+++ app/controllers/main_controller.rb
@@ -0,0 +1,6 @@
+class MainController < ApplicationController
+    def index 
+        # flash.now[:notice] = "Log in successfully"
+        # flash.now[:alert] = "Invalid credentials"
+    end
+end 
